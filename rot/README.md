c4970e6b151c42b7ba46f86b723960170e180ffa
Ian Klatzco <iklatzco@gmail.com>
2020-10-16 01:03:22 -0700

    e

--- rot/README.md
+++ rot/README.md
@@ -1,15 +1,36 @@
-quick experiments in learning about python packaging, and rewriting rotN
-code i've now written on several occasions
+tools for doing rot13 quickly (because i've written this code \>ten times)
+
+also an exercise in learning about python packaging
 
 ```bash
 usage:
-  export PYTHONPATH="$HOME/py:$PYTHONPATH" # put in in zshrc
-  
+  # get code
   git clone https://github.com/ianklatzco/py
 
-  python ~/py/rot             # for CLI tool; pipe, filename, or string literal
+  # to us as use CLI tool
+  # accepts from stdin, filename, or argument
+  python ~/py/rot
+
+	ROTs 0 to 25
+	usage:
+	  python rotn.py STRING
+	  python rotn.py FILENAME
+	  printf 'foo' | python rotn.py
+
+
+  # alternatively, import as library
+
+  # put on path so you can use within python
+  export PYTHONPATH="$HOME/py:$PYTHONPATH" # put in in zshrc
+  
   python -i -c 'import rot'   # for repl usage
+  >>> rot.rot13('foo')
 ```
 
+learned:
+* [`__main__.py`](__main__.py) is for when you `python foldername/` (to invoke code like
+CLI)
+* [`__init__.py`](__init__.py) is for specifying exports and populates help() on REPL
 
-see [`__init__.py`](__init__.py) for summary of what i learned about packaging.
+see linked files for more detail
+
--- rot/__init__.py
+++ rot/__init__.py
@@ -3,20 +3,21 @@
 # ^ this is shown in pydoc/help
 
 # __all__ = ['rotn','rot13'] # customizes from rot import * and changes what pydoc shows
-# used to explicitly specific exports
+# used to explicitly specify exports
 
-# the . specifies local dir i think
+# i think the . specifies local dir
 from ._rot import rotn
 from ._rot import rot13
 
-# the following are possible as well; they make tab completion in the repl not
-# show a "rot" name. however, if one wishes to use the code directly, this is
-# somewhat clunky, since it requires a python ~/py/rot/_rot.py
+# naming your primary code file _rot.py (note the _) makes tab completion in
+# the repl not show a "rot" name, which keeps it slightly cleaner. however, if
+# one wishes to use the code directly, this is somewhat clunky, since it
+# requires a python ~/py/rot/_rot.py
 # from ._rot import rotn
 # from ._rot import rot13
 
 # HOWEVER, i learned that you can put a __main__.py.
 # this enables you to simple python ~/py/rot to get the script's CLI.
-# eventually i will probably package this to resemble something like pwnlib, i suspect.
+# eventually i will probably package this to resemble something like pwnlib, i suspect. (kitchen sink is good.)
 # i think i want this to be a CLI lib more than anything else; quick scripts and tooling only.
 # anything resembling a real library or piece of software should not depend on any of this code; i expect it to be too much in flux.
