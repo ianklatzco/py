afa61082dc59e76f61ddc5e7c4e7b9774a2132e6
Ian Klatzco <iklatzco@gmail.com>
2020-10-16 00:47:38 -0700

    second commit

--- rot/README.md
+++ rot/README.md
@@ -1,2 +1,13 @@
 quick experiments in learning about python packaging, and rewriting rotN
 code i've now written on several occasions
+
+usage:
+  export PYTHONPATH="$HOME/py:$PYTHONPATH" # put in in zshrc
+  
+  git clone https://github.com/ianklatzco/py
+
+  python ~/py/rot             # for CLI tool; pipe, filename, or string literal
+  python -i -c 'import rot'   # for repl usage
+
+
+see __init__.py for summary of what i learned about packaging.
--- rot/__init__.py
+++ rot/__init__.py
@@ -6,11 +6,17 @@
 # used to explicitly specific exports
 
 # the . specifies local dir i think
-from .rot import rotn
-from .rot import rot13
+from ._rot import rotn
+from ._rot import rot13
 
 # the following are possible as well; they make tab completion in the repl not
 # show a "rot" name. however, if one wishes to use the code directly, this is
-# somewhat clunky.
+# somewhat clunky, since it requires a python ~/py/rot/_rot.py
 # from ._rot import rotn
 # from ._rot import rot13
+
+# HOWEVER, i learned that you can put a __main__.py.
+# this enables you to simple python ~/py/rot to get the script's CLI.
+# eventually i will probably package this to resemble something like pwnlib, i suspect.
+# i think i want this to be a CLI lib more than anything else; quick scripts and tooling only.
+# anything resembling a real library or piece of software should not depend on any of this code; i expect it to be too much in flux.
